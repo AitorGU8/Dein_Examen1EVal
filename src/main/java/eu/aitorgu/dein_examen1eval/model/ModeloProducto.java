16ae85f60f26b887418b3e375123861e968cd2c9
aitorgu <aitor.icj@gmail.com>
2024-11-08 09:53:18 +0100

    diseño creado

--- src/main/java/eu/aitorgu/dein_examen1eval/model/ModeloProducto.java
+++ src/main/java/eu/aitorgu/dein_examen1eval/model/ModeloProducto.java
@@ -1,4 +1,71 @@
 package eu.aitorgu.dein_examen1eval.model;
 
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import javax.swing.plaf.basic.BasicListUI;
+import java.sql.Blob;
+
 public class ModeloProducto {
+    private SimpleIntegerProperty codigo;
+    private SimpleStringProperty nombre;
+    private SimpleFloatProperty precio;
+    private SimpleIntegerProperty disponible;
+    private Blob imagen;
+
+    public ModeloProducto(int codigo, String nombre, float precio, int disponible, Blob imagen) {
+        this.codigo = new SimpleIntegerProperty(codigo);
+        this.nombre = new SimpleStringProperty(nombre);
+        this.precio = new SimpleFloatProperty(precio);
+        this.disponible =  new SimpleIntegerProperty(disponible);
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString() {
+        return "ModeloProducto{" +
+                "codigo=" + codigo +
+                ", nombre=" + nombre +
+                ", precio=" + precio +
+                ", disponible=" + disponible +
+                ", imagen=" + imagen +
+                '}';
+    }
+
+    public int getCodigo() {
+        return codigo.get();
+    }
+
+    public SimpleIntegerProperty codigoProperty() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre.get();
+    }
+
+    public SimpleStringProperty nombreProperty() {
+        return nombre;
+    }
+
+    public float getPrecio() {
+        return precio.get();
+    }
+
+    public SimpleFloatProperty precioProperty() {
+        return precio;
+    }
+
+    public int getDisponible() {
+        return disponible.get();
+    }
+
+    public SimpleIntegerProperty disponibleProperty() {
+        return disponible;
+    }
+
+    public Blob getImagen() {
+        return imagen;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module eu.aitorgu.dein_examen1eval {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens eu.aitorgu.dein_examen1eval to javafx.fxml;
